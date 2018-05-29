221bbfb3e1fa1e582363868b14b23a0f5314235d
Asia Stuerznickel <astuerznickel@gmail.com>
2018-05-29 13:13:07 +0100

    apprentices controller

---
+++ app/controllers/apprentices_controller.rb
@@ -0,0 +1,38 @@
+class ApprenticesController < ApplicationController
+  before_action :set_apprentice, only:[:show, :edit, :update]
+
+  def index
+    @apprentices = [""] if @apprentices == nil
+  end
+
+  def show
+  end
+
+  def new
+    @apprentice = Apprentice.new
+  end
+
+  def create
+    @apprentice = Apprentice.new(apprentice_params)
+    @apprentice.save
+    redirect_to apprentice_show(@apprentice)
+  end
+
+  def edit
+  end
+
+  def update
+    @apprentice.update(apprentice_params)
+    redirect_to apprentice_show(@apprentice)
+  end
+
+private
+  def set_apprentice
+    @apprentice = Apprentice.find(params[:id])
+  end
+
+  def apprentice_params
+    params.require(:apprentice).permit(:agency_id, :first_name, :last_name, :trade, :skills, :borough, :address, :personal_statement, :app_start_date, :app_end_date, :college_day, :hourly_rate_cents)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'apprentices/index'
+  get 'apprentice/show'
+  get 'apprentices/new'
+  get 'apprentices/create'
+  get 'apprentices/edit'
+  get 'apprentices/update'
   devise_for :users
   root to: 'pages#home'
 

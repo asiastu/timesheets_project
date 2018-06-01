a1fa46d6a599b37b953a709b11ca5eaba567de71
morgan krischer <morgan.krischer@gmail.com>
2018-06-01 22:31:25 +0100

    status added and refined user_can_see? method in apprentices#show

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -2,4 +2,6 @@ class Timesheet < ApplicationRecord
   belongs_to :placement
   has_many :timesheet_segments, dependent: :destroy
   has_many :expenses, dependent: :destroy
+
+  validates :status, inclusion: { in: ["Paid", "Accepted", "Rejected", "Queried", "Submitted", "Payment pending", "Pending Submission"] }
 end
---
+++ db/migrate/20180601192941_add_status_to_timesheets.rb
@@ -0,0 +1,5 @@
+class AddStatusToTimesheets < ActiveRecord::Migration[5.2]
+  def change
+    add_column :timesheets, :status, :string, default: "Pending Submission"
+  end
+end

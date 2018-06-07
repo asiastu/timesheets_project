0ae9625d31a1701b6dd0270d5e4dd992de9a3627
Asia Stuerznickel <astuerznickel@gmail.com>
2018-06-07 16:56:18 +0100

    appr dash wip

--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,6 @@ ActiveRecord::Schema.define(version: 2018_06_07_112715) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "hourly_rate_cents", default: 0, null: false
-    t.float "latitude"
-    t.float "longitude"
     t.index ["agency_id"], name: "index_apprentices_on_agency_id"
     t.index ["user_id"], name: "index_apprentices_on_user_id"
   end
